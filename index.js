0728336cd3019308384d32acf94771f5eee3dc6f
hp <sahalnurdin@gmail.com>
2022-10-06 11:29:21 +0700

    Third Commid

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// Digunakan untuk efek animasi saat muncul 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el) => observer.observe(el));
